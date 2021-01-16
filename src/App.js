48595c25057acb14bd5d9407b07fcb719a791311
sabbirdiu <sabbir.s.dk7@gmail.com>
2021-01-16 18:57:31 +0600

    added Navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-  
-} from "react-router-dom";
+import React from "react";
+import {BrowserRouter as Router,Route,Switch} from "react-router-dom";
 import  About from './pages/About'
 import  Details from './pages/Details'
 import  Error from './pages/Error'
 import  Home from './pages/Home'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route path='/details/:id' >
-              <Details />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/cocktail/:id">
+          <Details />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
+    <nav className="navbar">
+      <div className="nav-center">
+        <ul className="nav-links">
+            <li>
+                <Link to="/">JuiceBar</Link>
+            </li>
+        </ul>
+        <ul className="nav-links">
+          <li>
+            <Link to="/">home</Link>
+          </li>
+          <li>
+            <Link to="/about">about</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
     )
 }
 
